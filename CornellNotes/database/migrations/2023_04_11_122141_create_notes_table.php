535b6828c0bfa4d4059bf9d3d6ec837d8cf712f2
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-04-11 06:58:54 -0600

    CornellNotes

---
+++ CornellNotes/database/migrations/2023_04_11_122141_create_notes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->string('keyWords');
+            $table->string('summary');
+            $table->date('creation_date');
+
+            $table->foreignId('id_user')
+                ->constrained('users')
+                ->cascadeOnUpdate();
+            $table->foreignId('id_subject')
+                ->constrained('subjects')
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
---
+++ CornellNotes/database/migrations/2023_04_11_122200_create_reminders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->integer('value');
+            $table->date('creation_date');
+            $table->date('event_date');
+
+            $table->foreignId('id_user')
+                ->constrained('users')
+                ->cascadeOnUpdate();
+            $table->foreignId('id_subject')
+                ->constrained('subjects')
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reminders');
+    }
+};
