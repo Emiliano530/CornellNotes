fd26d8938de948e4e2be840a61a4842d79f9fdf1
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-04-25 08:00:00 -0600

    CornellNotes

--- CornellNotes/resources/views/components/delete-button-icon.blade.php
+++ CornellNotes/resources/views/components/delete-button-icon.blade.php
@@ -1,30 +1,7 @@
 <form method="POST" action="{{ $url ?? ''}}" accept-charset="UTF-8" class="form-delete">
-        {{ method_field('DELETE') }}
-        {{ csrf_field() }}
-    <button type="submit" title="Eliminar">
+    {{ method_field('DELETE') }}
+    {{ csrf_field() }}
+    <button type="submit" title="Eliminar" onclick="return confirm(&quot;¿Seguro de querer eliminarlo?&quot;)">
         {{ $slot }}
     </button>
 </form>
-
-<script>
-    $('.form-delete').submit(function(e){
-        e.preventDefault()
-
-        Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-        if (result.isConfirmed) {
-            this.submit()
-        }
-        })
-
-    });
-
-    
-</script>
